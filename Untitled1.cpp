8e4abefd622abe592cecd4526f5a7a3edbe0a3f8
BaiYuhao_SpiceeYJ <45082766+BaiYuhaoSpiceeYJ@users.noreply.github.com>
2018-11-16 15:33:58 +0100

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*cout<<"     --        --"<<endl;
+    cout<<"   /     \\   /     \\ " <<endl;
+    cout<<"      y         u"<<endl;
+    cout<<"   \\     /   \\     /"<<endl;
+    cout<<"    --           --"<<endl;
+
+
+    cout<<" /     \\       /     \\ " <<endl;
+    cout<<"     g     h     j"<<endl;
+    cout<<" \\     /       \\     /"<<endl;
+
+
+
+    cout<<"    --           --"<<endl;
+    cout<<"   /     \\   /     \\ " <<endl;
+    cout<<"      b         n"<<endl;
+    cout<<"   \\     /   \\     /"<<endl;
+    cout<<"      --        --"<<endl;*/
+
+    cout<<"     /  \\  /  \\ " <<endl;
+    cout<<"    |  y  |  u |"<<endl;
+    cout<<"   / \\   / \\  /  \\ "<<endl;
+
+
+    cout<<"  | g  |  h  |  j |"<<endl;
+    cout<<"   \\ /   \\ /  \\  /"<<endl;
+
+    cout<<"    |  b  |  n |"<<endl;
+    cout<<"     \\   / \\  /"<<endl;
+    return 0;
+}
