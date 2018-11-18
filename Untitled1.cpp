1157fee2b290c51763715474bfea39d83819e9c7
BaiYuhao_SpiceeYJ <45082766+BaiYuhaoSpiceeYJ@users.noreply.github.com>
2018-11-18 21:21:40 +0100

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//边长1 
+    /*cout<<"     /   \\ /  \\ " <<endl;
+    cout<<"    |  y  |  u |"<<endl;
+    cout<<"   / \\   / \\  /  \\ "<<endl;
+
+
+    cout<<"  | g  |  h  |  j |"<<endl;
+    cout<<"   \\ /   \\ /  \\  /"<<endl;
+
+    cout<<"    |  b  |  n |"<<endl;
+    cout<<"     \\   / \\  /"<<endl;*/
+    
+    //边长2 
+    /*cout<<"        /\\     /\\" <<endl;
+    cout<<"      /    \\ /    \\ " <<endl;
+    cout<<"     |   y  |   u  |"<<endl;
+    cout<<"     |      |      |"<<endl;
+    cout<<"    /\\     / \\     /\\ "<<endl;
+	cout<<"  /    \\ /     \\ /    \\ "<<endl;
+
+
+    cout<<" |   g  |   h   |  j   |"<<endl;
+    cout<<" |      |       |      |"<<endl;
+    cout<<"  \\    / \\     / \\    /"<<endl;
+    cout<<"    \\/     \\ /     \\/"<<endl;
+
+    cout<<"     |   b  |   n  |"<<endl;
+    cout<<"     |      |      |"<<endl;
+    cout<<"      \\    / \\    /"<<endl;
+    cout<<"        \\/     \\/"<<endl;*/
+    
+    //边长3
+    /*cout<<"             /\\         /\\" <<endl;
+    cout<<"           /    \\     /    \\ " <<endl;
+    cout<<"         /        \\ /        \\ " <<endl;
+    cout<<"        |          |          |"<<endl;
+    cout<<"        |     Y    |     U    |"<<endl;
+    cout<<"        |          |          |"<<endl;
+    cout<<"       / \\        / \\        / \\ "<<endl;
+    cout<<"     /     \\    /     \\    /     \\ "<<endl;
+	cout<<"   /         \\/         \\/         \\ "<<endl;
+	cout<<"  |          |          |          |"<<endl;
+	cout<<"  |     G    |     H    |     J    |"<<endl;
+    cout<<"  |          |          |          |"<<endl;
+    cout<<"   \\         /\\         /\\         /"<<endl;
+    cout<<"     \\     /    \\     /    \\     /"<<endl;
+    cout<<"       \\ /        \\ /        \\ /"<<endl;
+	cout<<"        |          |          |"<<endl;
+    cout<<"        |     B    |     N    |"<<endl;
+    cout<<"        |          |          |"<<endl;
+    cout<<"         \\        / \\        /"<<endl;
+    cout<<"     	   \\    /     \\    /"<<endl;
+    cout<<"      	     \\/         \\/"<<endl;*/
+    
+    
+    //边长3*
+    cout<<"              A          B" <<endl;
+    cout<<"           /    \\     /    \\ " <<endl;
+    cout<<"        C          D          E " <<endl;
+    cout<<"        |          |          |"<<endl;
+    cout<<"        |   Mine   |  Forest  |"<<endl;
+    cout<<"        |     4    |     8    |"<<endl;
+    cout<<"        F          G          H "<<endl;
+    cout<<"     /     \\    /     \\    /     \\ "<<endl;
+	cout<<"   I         J          K          L "<<endl;
+	cout<<"  |          |  Robber  |          |"<<endl;
+	cout<<"  |   Brick  |  Desert  |   Wheat  |"<<endl;
+    cout<<"  |     6    |    11    |     10   |"<<endl;
+    cout<<"   M         N          O          P"<<endl;
+    cout<<"     \\     /    \\     /    \\     /"<<endl;
+    cout<<"        Q          R          S"<<endl;
+	cout<<"        |          |          |"<<endl;
+    cout<<"        |  Grass   |  Forest  |"<<endl;
+    cout<<"        |     9    |     2    |"<<endl;
+    cout<<"        T          U          V"<<endl;
+    cout<<"     	   \\    /     \\    /"<<endl;
+    cout<<"      	     W           X"<<endl;
+    return 0;
+}
